cfb531b0a9646b6ff7963172a8a91b1b3f395aca
BuddyCornwall <131293169+BuddyCornwall@users.noreply.github.com>
2024-03-05 14:18:40 +0000

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -2794,25 +2794,6 @@ pokemon.setItem(item);
 name: "Pickup",
 },
 
-pixilate: {
-onModifyTypePriority: -1,
-onModifyType(move, pokemon) {
-const noModifyType = [
-'judgment', 'multiattack', 'naturalgift', 'revelationdance', 'technoblast', 'terrainpulse', 'weatherball',
-];
-if (move.type === 'Normal' && !noModifyType.includes(move.id) &&
-!(move.isZ && move.category !== 'Status') && !(move.name === 'Tera Blast' && pokemon.terastallized)) {
-move.type = 'Fairy';
-move.typeChangerBoosted = this.effect;
-}
-},
-onBasePowerPriority: 23,
-onBasePower(basePower, pokemon, target, move) {
-if (move.typeChangerBoosted === this.effect) return this.chainModify([4915, 4096]);
-},
-name: "Pixilate",
-},
-
 plus: {
 onModifySpAPriority: 5,
 onModifySpA(spa, pokemon) {
