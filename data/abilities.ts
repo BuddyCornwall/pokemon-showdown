e14c2b1bc4f239ab754c1943e2767e39d0dd3ccf
BuddyCornwall <131293169+BuddyCornwall@users.noreply.github.com>
2024-03-05 14:11:17 +0000

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -4623,7 +4623,7 @@ name: "Toxic Debris",
 spikedebris: {
 onDamagingHit(damage, target, source, move) {
 const side = source.isAlly(target) ? source.side.foe : source.side;
-const toxicSpikes = side.sideConditions['spikes'];
+const spikes= side.sideConditions['spikes'];
 if (move.category === 'Physical' && (!Spikes || Spikes.layers < 2)) {
 this.add('-activate', target, 'ability: Spike Debris');
 side.addSideCondition('spikes', target);
@@ -4635,7 +4635,7 @@ name: "Spike Debris",
 stealthyspikedebris: {
 onDamagingHit(damage, target, source, move) {
 const side = source.isAlly(target) ? source.side.foe : source.side;
-const stealthrock = side.sideConditions: 'stealthrock',
+const stealthrock= side.sideConditions: 'stealthrock',
 if (move.category === 'Physical' && (!stealthrock|| stealthrock.layers < 2)) {
 this.add('-activate', target, 'ability: 'Stealthy Spike Debris');
 side.addSideCondition('stealthrock', target);
@@ -4647,7 +4647,7 @@ name: "Stealthy Spike Debris",
 webslingers: {
 onDamagingHit(damage, target, source, move) {
 const side = source.isAlly(target) ? source.side.foe : source.side;
-const toxicSpikes = side.sideConditions: 'stickyweb',
+const stickyweb= side.sideConditions: 'stickyweb',
 if (move.category === 'Physical' && (!stickyweb|| stickyweb.layers < 2)) {
 this.add('-activate', target, 'ability: 'Web Slingers');
 side.addSideCondition('stickyweb', target);
