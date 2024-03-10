0e067615031fb8495d69167b1f3c9f5beafdf132
BuddyCornwall <131293169+BuddyCornwall@users.noreply.github.com>
2024-03-10 16:17:07 +0000

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -2875,7 +2875,6 @@ onModifyDefPriority: 6,
 onModifyDef(def, pokemon) {
 if (pokemon.status) {
 return this.chainModify(1.5);
-}
 onModifySPD(spd, pokemon) {
 if (pokemon.status) {
 return this.chainModify(1.5);
