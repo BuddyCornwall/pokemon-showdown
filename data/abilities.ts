56074bec00c1095ec4784c610cd0bc1197a78b92
BuddyCornwall <131293169+BuddyCornwall@users.noreply.github.com>
2024-03-10 15:40:54 +0000

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -83,7 +83,7 @@ dwaynelate: {
 onModifyTypePriority: -1,
 onModifyType(move, pokemon) {
 const noModifyType = [
-judgment', 'multiattack', 'naturalgift', 'revelationdance', 'technoblast', 'terrainpulse', 'weatherball',
+'judgment', 'multiattack', 'naturalgift', 'revelationdance', 'technoblast', 'terrainpulse', 'weatherball',
 ];
 if (move.type === 'Normal' && !noModifyType.includes(move.id) &&
 !(move.isZ && move.category !== 'Status') && !(move.name === 'Tera Blast' && pokemon.terastallized)) {
