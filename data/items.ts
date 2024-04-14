aa128ba66224bae2e384b79d42154136af1193e0
BuddyCornwall <131293169+BuddyCornwall@users.noreply.github.com>
2024-04-14 14:27:20 +0100

    Update items.ts

--- data/items.ts
+++ data/items.ts
@@ -7171,19 +7171,24 @@ isBreakable: true,
 
 egg: {
 name: "Egg",
-onDamage: function (damage, target, source, effect) {
-if (effect && effect.effectType === 'Move' && source && source !== target && !target.ateBerry) {
+onDamagingHit: function (damage, target, source, move) {
 this.add('-activate', target, 'item: Egg');
 source.addVolatile('confusion', target);
 let damageAmount = Math.ceil(source.maxhp / 10);
 this.damage(damageAmount, source, target);
-target.consumeItem();
+target.setItem('');
+},
+onStart: function (pokemon) {
+this.effectData.used = false;
+},
+onAfterUseItem: function (item, pokemon) {
+if (!this.effectData.used) {
+this.effectData.used = true;
+pokemon.setItem('');
 }
 },
-onEat: function (pokemon) {
-pokemon.removeItem();
 },
-},
+
 
 
 };
