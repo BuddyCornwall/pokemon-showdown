0c65d45b95f447ea60fe429a9551a73b99a87469
BuddyCornwall <131293169+BuddyCornwall@users.noreply.github.com>
2024-04-15 17:40:51 +0100

    Update conditions.ts

--- data/conditions.ts
+++ data/conditions.ts
@@ -714,29 +714,31 @@ return 8;
 }
 return 5;
 },
-onModifyDefPriority: 10,
-onModifyDef(def, pokemon) {
-if (pokemon.hasType('Ice') && this.field.isWeather('snow')) {
-return this.modify(def, 1.5);
-}
-},
 onFieldStart(field, source, effect) {
 if (effect?.effectType === 'Ability') {
 if (this.gen <= 5) this.effectState.duration = 0;
-this.add('-weather', 'Snow', '[from] ability: ' + effect.name, '[of] ' + source);
+this.add('-weather', 'Snow Hail', '[from] ability: ' + effect.name, '[of] ' + source);
 } else {
-this.add('-weather', 'Snow');
+this.add('-weather', 'Snow Hail');
 }
 },
 onFieldResidualOrder: 1,
 onFieldResidual() {
-this.add('-weather', 'Snow', '[upkeep]');
-if (this.field.isWeather('snow')) this.eachEvent('Weather');
+this.add('-weather', 'Snow Hail', '[upkeep]');
+if (this.field.isWeather('snowhail')) this.eachEvent('Weather');
+},
+onWeather(target) {
+this.damage(target.baseMaxhp / 16);
+const pokemon = this.effectData.target;
+if (pokemon.hasType('Ice')) {
+this.boost({def: 1,});
+}
 },
 onFieldEnd() {
 this.add('-weather', 'none');
 },
-},
+}
+
 
 deltastream: {
 name: 'DeltaStream',
