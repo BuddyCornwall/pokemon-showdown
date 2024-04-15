4008fd9d131f36859632430786439237a40266a3
BuddyCornwall <131293169+BuddyCornwall@users.noreply.github.com>
2024-04-15 20:37:10 +0100

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -5021,7 +5021,9 @@ name: "Flip Turn",
 pp: 1.25,
 priority: 0,
 flags: {contact: 1, protect: 1, mirror: 1},
+{
 this.add('-message', "don't go!, pokemon)
+},
 selfSwitch: true,
 secondary: null,
 target: "any",
