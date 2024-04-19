4dec6802dfaca1a8f5d6a38ced49c99dda84f7d8
BuddyCornwall <131293169+BuddyCornwall@users.noreply.github.com>
2024-04-19 21:04:55 +0100

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -10030,52 +10030,41 @@ type: "Flying",
 },
 
 perishsong: {
-accuracy: 95,
-basePower: 0,
-category: "Status",
-name: "Perish Song",
-pp: 0.625,
-priority: 1,
-flags: {sound: 1, distance: 1, bypasssub: 1},
-onHitField(target, source, move) {
-let result = false;
-let message = false;
-for (const pokemon of this.getAllActive()) {
-if (this.runEvent('Invulnerability', pokemon, source, move) === false) {
-this.add('-miss', source, pokemon);
-result = true;
-} else if (this.runEvent('TryHit', pokemon, source, move) === null) {
-result = true;
-} else if (!pokemon.volatiles['perishsong']) {
-// Generate a random duration between 2 and 5 turns
-const duration = this.random(2, 5);
-pokemon.addVolatile('perishsong', pokemon, move, 'perishsong', duration);
-result = true;
-message = true;
-}
-}
-if (!result) return false;
-if (message) this.add('-fieldactivate', 'move: Perish Song');
+    accuracy: 95,
+    basePower: 0,
+    category: "Status",
+    name: "Perish Song",
+    pp: 5,
+    priority: 0,
+    flags: {sound: 1, distance: 1, bypasssub: 1},
+    onHit(target, source, move) {
+        let duration = this.random(2, 5); // Random duration between 2 and 5 turns
+        this.add('-start', target, 'perish' + duration);
+        target.addVolatile('perishsong', target, move, 'perishsong', duration);
+        this.add('-fieldactivate', 'move: Perish Song');
+    },
+    condition: {
+        duration: 5, // Maximum duration, it will be adjusted individually for each Pokémon
+        onStart(target) {
+            this.add('-start', target, 'perish3');
+        },
+        onResidualOrder: 4,
+        onResidual(pokemon) {
+            const duration = pokemon.volatiles['perishsong'].duration;
+            if (duration > 0) {
+                this.add('-start', pokemon, 'perish' + duration);
+                pokemon.volatiles['perishsong'].duration--;
+            } else {
+                this.add('-start', pokemon, 'perish0');
+                pokemon.faint();
+            }
+        },
+    },
+    secondary: null,
+    target: "all",
+    type: "Normal",
 },
-condition: {
-duration: 5,
-onStart(target) {
-this.add('-start', target, 'perish3');
-},
-onEnd(target) {
-this.add('-start', target, 'perish0');
-target.faint();
-},
-onResidualOrder: 24,
-onResidual(pokemon) {
-const duration = pokemon.volatiles['perishsong'].duration;
-this.add('-start', pokemon, 'perish' + duration);
-},
-},
-secondary: null,
-target: "all",
-type: "Normal",
-},
+
 
 petalblizzard: {
 accuracy: 95,
