f0d43c51aacb6b4a839113b173ada8ffc950eee5
BuddyCornwall <131293169+BuddyCornwall@users.noreply.github.com>
2024-07-08 15:38:34 +0100

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -3058,9 +3058,7 @@ onHit(target, source, move) {
 let success = false;
 if (!target.volatiles['substitute'] || move.infiltrates) success = !!this.boost({evasion: -1});
 const removeTarget = ['reflect', 'lightscreen', 'auroraveil', 'safeguard', 'mist', 'spikes', 'toxicspikes', 'stealthrock', 'stickyweb', 'gmaxsteelsurge',];
-const removeAll = [
-spikes', 'toxicspikes', 'stealthrock', 'stickyweb', 'gmaxsteelsurge',
-];
+const removeAll = ['spikes', 'toxicspikes', 'stealthrock', 'stickyweb', 'gmaxsteelsurge',];
 for (const targetCondition of removeTarget) {
 if (target.side.removeSideCondition(targetCondition)) {
 if (!removeAll.includes(targetCondition)) continue;
