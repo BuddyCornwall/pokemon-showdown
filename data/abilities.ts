68035c26628a246de442e0734c6d52b8d4cd6d64
BuddyCornwall <131293169+BuddyCornwall@users.noreply.github.com>
2025-04-22 15:33:54 +0100

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -3715,71 +3715,71 @@ name: "Purifying Salt",
 },
 
 quarkdrive: {
-		onSwitchInPriority: -2,
-		onStart(pokemon) {
-			this.singleEvent('TerrainChange', this.effect, this.effectState, pokemon);
-		},
-		onTerrainChange(pokemon) {
-			if (this.field.isTerrain('electricterrain')) {
-				pokemon.addVolatile('quarkdrive');
-			} else if (!pokemon.volatiles['quarkdrive']?.fromBooster) {
-				pokemon.removeVolatile('quarkdrive');
-			}
-		},
-		onEnd(pokemon) {
-			delete pokemon.volatiles['quarkdrive'];
-			this.add('-end', pokemon, 'Quark Drive', '[silent]');
-		},
-		condition: {
-			noCopy: true,
-			onStart(pokemon, source, effect) {
-				if (effect?.name === 'Booster Energy') {
-					this.effectState.fromBooster = true;
-					this.add('-activate', pokemon, 'ability: Quark Drive', '[fromitem]');
-				} else {
-					this.add('-activate', pokemon, 'ability: Quark Drive');
-				}
-				this.effectState.bestStat = pokemon.getBestStat(false, true);
-				this.add('-start', pokemon, 'quarkdrive' + this.effectState.bestStat);
-			},
-			onModifyAtkPriority: 5,
-			onModifyAtk(atk, pokemon) {
-				if (this.effectState.bestStat !== 'atk' || pokemon.ignoringAbility()) return;
-				this.debug('Quark Drive atk boost');
-				return this.chainModify([5325, 4096]);
-			},
-			onModifyDefPriority: 6,
-			onModifyDef(def, pokemon) {
-				if (this.effectState.bestStat !== 'def' || pokemon.ignoringAbility()) return;
-				this.debug('Quark Drive def boost');
-				return this.chainModify([5325, 4096]);
-			},
-			onModifySpAPriority: 5,
-			onModifySpA(spa, pokemon) {
-				if (this.effectState.bestStat !== 'spa' || pokemon.ignoringAbility()) return;
-				this.debug('Quark Drive spa boost');
-				return this.chainModify([5325, 4096]);
-			},
-			onModifySpDPriority: 6,
-			onModifySpD(spd, pokemon) {
-				if (this.effectState.bestStat !== 'spd' || pokemon.ignoringAbility()) return;
-				this.debug('Quark Drive spd boost');
-				return this.chainModify([5325, 4096]);
-			},
-			onModifySpe(spe, pokemon) {
-				if (this.effectState.bestStat !== 'spe' || pokemon.ignoringAbility()) return;
-				this.debug('Quark Drive spe boost');
-				return this.chainModify(1.5);
-			},
-			onEnd(pokemon) {
-				this.add('-end', pokemon, 'Quark Drive');
-			},
-		},
-		flags: { failroleplay: 1, noreceiver: 1, noentrain: 1, notrace: 1, failskillswap: 1, notransform: 1 },
-		name: "Quark Drive",
-		rating: 3,
-		num: 282,
-	},
+onSwitchInPriority: -2,
+onStart(pokemon) {
+this.singleEvent('TerrainChange', this.effect, this.effectState, pokemon);
+},
+onTerrainChange(pokemon) {
+if (this.field.isTerrain('electricterrain')) {
+pokemon.addVolatile('quarkdrive');
+} else if (!pokemon.volatiles['quarkdrive']?.fromBooster) {
+pokemon.removeVolatile('quarkdrive');
+}
+},
+onEnd(pokemon) {
+delete pokemon.volatiles['quarkdrive'];
+this.add('-end', pokemon, 'Quark Drive', '[silent]');
+},
+condition: {
+noCopy: true,
+onStart(pokemon, source, effect) {
+if (effect?.name === 'Booster Energy') {
+this.effectState.fromBooster = true;
+this.add('-activate', pokemon, 'ability: Quark Drive', '[fromitem]');
+} else {
+this.add('-activate', pokemon, 'ability: Quark Drive');
+}
+this.effectState.bestStat = pokemon.getBestStat(false, true);
+this.add('-start', pokemon, 'quarkdrive' + this.effectState.bestStat);
+},
+onModifyAtkPriority: 5,
+onModifyAtk(atk, pokemon) {
+if (this.effectState.bestStat !== 'atk' || pokemon.ignoringAbility()) return;
+this.debug('Quark Drive atk boost');
+return this.chainModify([5325, 4096]);
+},
+onModifyDefPriority: 6,
+onModifyDef(def, pokemon) {
+if (this.effectState.bestStat !== 'def' || pokemon.ignoringAbility()) return;
+this.debug('Quark Drive def boost');
+return this.chainModify([5325, 4096]);
+},
+onModifySpAPriority: 5,
+onModifySpA(spa, pokemon) {
+if (this.effectState.bestStat !== 'spa' || pokemon.ignoringAbility()) return;
+this.debug('Quark Drive spa boost');
+return this.chainModify([5325, 4096]);
+},
+onModifySpDPriority: 6,
+onModifySpD(spd, pokemon) {
+if (this.effectState.bestStat !== 'spd' || pokemon.ignoringAbility()) return;
+this.debug('Quark Drive spd boost');
+return this.chainModify([5325, 4096]);
+},
+onModifySpe(spe, pokemon) {
+if (this.effectState.bestStat !== 'spe' || pokemon.ignoringAbility()) return;
+this.debug('Quark Drive spe boost');
+return this.chainModify(1.5);
+},
+onEnd(pokemon) {
+this.add('-end', pokemon, 'Quark Drive');
+},
+},
+flags: { failroleplay: 1, noreceiver: 1, noentrain: 1, notrace: 1, failskillswap: 1, notransform: 1 },
+name: "Quark Drive",
+rating: 3,
+num: 282,
+},
 
 queenlymajesty: {
 onFoeTryMove(target, source, move) {
