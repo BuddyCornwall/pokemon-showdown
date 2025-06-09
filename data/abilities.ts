99f1bd9a3d8719ac6bac6edc36d3616e620d2352
BuddyCornwall <131293169+BuddyCornwall@users.noreply.github.com>
2025-06-09 15:53:13 +0100

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -3612,43 +3612,28 @@ this.add('-end', pokemon, 'Protosynthesis', '[silent]');
 },
 condition: {
 noCopy: true,
-onStart(pokemon, source, effect) {
-if (effect?.id === 'boosterenergy') {
-this.effectState.fromBooster = true;
-this.add('-activate', pokemon, 'ability: Protosynthesis', '[fromitem]');
-} else {
-this.add('-activate', pokemon, 'ability: Protosynthesis');
-}
-this.effectState.bestStat = pokemon.getBestStat(false, true);
-this.add('-start', pokemon, 'protosynthesis' + this.effectState.bestStat);
-},
 onModifyAtkPriority: 5,
 onModifyAtk(atk, source, target, move) {
 if (this.effectState.bestStat !== 'atk') return;
-this.debug('Protosynthesis atk boost');
 return this.chainModify([150, 100]);
 },
 onModifyDefPriority: 6,
 onModifyDef(def, target, source, move) {
 if (this.effectState.bestStat !== 'def') return;
-this.debug('Protosynthesis def boost');
 return this.chainModify([150, 100]);
 },
 onModifySpAPriority: 5,
 onModifySpA(relayVar, source, target, move) {
 if (this.effectState.bestStat !== 'spa') return;
-this.debug('Protosynthesis spa boost');
 return this.chainModify([150, 100]);
 },
 onModifySpDPriority: 6,
 onModifySpD(relayVar, target, source, move) {
 if (this.effectState.bestStat !== 'spd') return;
-this.debug('Protosynthesis spd boost');
 return this.chainModify([150, 100]);
 },
 onModifySpe(spe, pokemon) {
 if (this.effectState.bestStat !== 'spe') return;
-this.debug('Protosynthesis spe boost');
 return this.chainModify([150, 100]);
 },
 onEnd(pokemon) {
