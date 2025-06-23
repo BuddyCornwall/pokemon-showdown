dbeca04e48b089ba4701c65f6e17e5609532e461
BuddyCornwall <131293169+BuddyCornwall@users.noreply.github.com>
2025-06-23 12:41:08 +0100

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -4,21 +4,18 @@ section: "BUDPOW",
 },
 {
 name: "[PGL: Singles] BUDPOW",
-mod: 'gen9',
 gameType: 'singles',
 ruleset: ['Picked Team Size = 3','Max Move Count = 8', 'HP Percentage Mod', 'Cancel Mod', 'VGC Timer', 'Dynamax Clause', 'Team Preview', 'Min Team Size = 3', 'Max Team Size = 3'],
 },
 
 {
 name: "[PGL: Doubles] BUDPOW",
-mod: 'gen9',
 gameType: 'doubles',
 ruleset: ['Picked Team Size = 4','Max Move Count = 8', 'HP Percentage Mod', 'Cancel Mod', 'VGC Timer', 'Dynamax Clause', 'Team Preview', 'Min Team Size = 4', 'Max Team Size = 4'],
 },
 
 {
 name: "[TRI*] BUDPOW",
-mod: 'gen9',
 gameType: 'triples',
 ruleset: ['Max Move Count = 8', 'HP Percentage Mod', 'Cancel Mod', 'Dynamax Clause'],
 
@@ -26,14 +23,12 @@ ruleset: ['Max Move Count = 8', 'HP Percentage Mod', 'Cancel Mod', 'Dynamax Clau
 
 {
 name: "[Gold Cup] BUDPOW",
-mod: 'gen9',
 gameType: 'singles',
 ruleset: ['Picked Team Size = 1','Max Move Count = 8', 'HP Percentage Mod', 'Cancel Mod', 'VGC Timer', 'Dynamax Clause', 'Team Preview', 'Min Team Size = 1', 'Max Team Size = 1'],
 },
 
 {
 name: "[MBSFBC] BUDPOW",
-mod: 'gen9',
 gameType: 'singles',
 ruleset: ['Max Move Count = 16', 'HP Percentage Mod', 'Cancel Mod', 'VGC Timer', 'Dynamax Clause', 'Team Preview', 'Min Team Size = 3', 'Max Team Size = 18'],
 },
@@ -48,7 +43,6 @@ threads: [
 `&bullet; <a href="https://www.smogon.com/forums/threads/3712619/">Random Battle Suggestions</a>`,
 ],
 searchShow: false,
-mod: 'gen9',
 team: 'random',
 ruleset: ['PotD', 'Obtainable', 'Species Clause', 'HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod'],
 },
