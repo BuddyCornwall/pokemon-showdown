8d3e0eee8a0f47f50866bd35d0f5247d4b3dcef8
BuddyCornwall <131293169+BuddyCornwall@users.noreply.github.com>
2025-06-23 12:43:52 +0100

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -3,34 +3,33 @@ export const Formats: FormatList = [
 section: "BUDPOW",
 },
 {
-name: "[PGL: Singles] BUDPOW",
+name: "[PGL: Singles]",
 gameType: 'singles',
-ruleset: ['Picked Team Size = 3','Max Move Count = 8', 'HP Percentage Mod', 'Cancel Mod', 'VGC Timer', 'Dynamax Clause', 'Team Preview', 'Min Team Size = 3', 'Max Team Size = 3'],
+ruleset: ['Picked Team Size = 3','Max Move Count = 8', 'HP Percentage Mod', 'Cancel Mod', 'Dynamax Clause', 'Team Preview', 'Min Team Size = 3', 'Max Team Size = 3'],
 },
 
 {
-name: "[PGL: Doubles] BUDPOW",
+name: "[PGL: Doubles]",
 gameType: 'doubles',
-ruleset: ['Picked Team Size = 4','Max Move Count = 8', 'HP Percentage Mod', 'Cancel Mod', 'VGC Timer', 'Dynamax Clause', 'Team Preview', 'Min Team Size = 4', 'Max Team Size = 4'],
+ruleset: ['Picked Team Size = 4','Max Move Count = 8', 'HP Percentage Mod', 'Cancel Mod', 'Dynamax Clause', 'Team Preview', 'Min Team Size = 4', 'Max Team Size = 4'],
 },
 
 {
-name: "[TRI*] BUDPOW",
+name: "[TRISTAN]",
 gameType: 'triples',
 ruleset: ['Max Move Count = 8', 'HP Percentage Mod', 'Cancel Mod', 'Dynamax Clause'],
-
 },
 
 {
-name: "[Gold Cup] BUDPOW",
+name: "[Gold Cup]",
 gameType: 'singles',
-ruleset: ['Picked Team Size = 1','Max Move Count = 8', 'HP Percentage Mod', 'Cancel Mod', 'VGC Timer', 'Dynamax Clause', 'Team Preview', 'Min Team Size = 1', 'Max Team Size = 1'],
+ruleset: ['Picked Team Size = 1','Max Move Count = 8', 'HP Percentage Mod', 'Cancel Mod', 'Dynamax Clause', 'Team Preview', 'Min Team Size = 1', 'Max Team Size = 1'],
 },
 
 {
-name: "[MBSFBC] BUDPOW",
+name: "[MBSFBC]",
 gameType: 'singles',
-ruleset: ['Max Move Count = 16', 'HP Percentage Mod', 'Cancel Mod', 'VGC Timer', 'Dynamax Clause', 'Team Preview', 'Min Team Size = 3', 'Max Team Size = 18'],
+ruleset: ['Max Move Count = 16', 'HP Percentage Mod', 'Cancel Mod', 'Dynamax Clause', 'Team Preview', 'Min Team Size = 3', 'Max Team Size = 18'],
 },
 
 {
