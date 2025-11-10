671135e44b78349f46da2ce42a421592f3b4078b
BuddyCornwall <131293169+BuddyCornwall@users.noreply.github.com>
2025-11-10 12:09:20 +0000

    Update items.ts

--- data/items.ts
+++ data/items.ts
@@ -977,54 +977,6 @@ return this.chainModify(0.5);
 onEat() { },
 },
 
-condition: {
-condition: {
-onStart(pokemon) {
-duration: 2,
-this.effectState.lastMove = '';
-onSourceAccuracy(accuracy, target, source, move) {
-this.effectState.numConsecutive = 0;
-if (!move.ohko) {
-},
-this.add('-enditem', source, 'Micle Berry');
-onTryMovePriority: -2,
-source.removeVolatile('micleberry');
-onTryMove(pokemon, target, move) {
-if (typeof accuracy === 'number') {
-if (!pokemon.hasItem('metronome')) {
-return this.chainModify([115, 100]);
-pokemon.removeVolatile('metronome');
-}
-return;
-}
-}
-},
-if (this.effectState.lastMove === move.id && pokemon.moveLastTurnResult) {
-},
-this.effectState.numConsecutive++;
-isNonstandard: "Unobtainable",
-} else if (pokemon.volatiles['twoturnmove']) {
-},
-if (this.effectState.lastMove !== move.id) {
-this.effectState.numConsecutive = 1;
-} else {
-this.effectState.numConsecutive++;
-}
-} else {
-this.effectState.numConsecutive = 0;
-}
-this.effectState.lastMove = move.id;
-},
-onModifyDamage(damage, source, target, move) {
-const dmgMod = [4096, 4915, 5734, 6553, 7372, 8192];
-const numConsecutive = this.effectState.numConsecutive > 5 ? 5 : this.effectState.numConsecutive;
-this.debug(`Current Metronome boost: ${dmgMod[numConsecutive]}/4096`);
-return this.chainModify([dmgMod[numConsecutive], 4096]);
-},
-},
-},
-
-
 cornnberry: {
 name: "Cornn Berry",
 isBerry: true,
@@ -3446,6 +3398,38 @@ basePower: 30,
 onStart(pokemon) {
 pokemon.addVolatile('metronome');
 },
+condition: {
+onStart(pokemon) {
+this.effectState.lastMove = '';
+this.effectState.numConsecutive = 0;
+},
+onTryMovePriority: -2,
+onTryMove(pokemon, target, move) {
+if (!pokemon.hasItem('metronome')) {
+pokemon.removeVolatile('metronome');
+return;
+}
+if (this.effectState.lastMove === move.id && pokemon.moveLastTurnResult) {
+this.effectState.numConsecutive++;
+} else if (pokemon.volatiles['twoturnmove']) {
+if (this.effectState.lastMove !== move.id) {
+this.effectState.numConsecutive = 1;
+} else {
+this.effectState.numConsecutive++;
+}
+} else {
+this.effectState.numConsecutive = 0;
+}
+this.effectState.lastMove = move.id;
+},
+onModifyDamage(damage, source, target, move) {
+const dmgMod = [4096, 4915, 5734, 6553, 7372, 8192];
+const numConsecutive = this.effectState.numConsecutive > 5 ? 5 : this.effectState.numConsecutive;
+this.debug(`Current Metronome boost: ${dmgMod[numConsecutive]}/4096`);
+return this.chainModify([dmgMod[numConsecutive], 4096]);
+},
+},
+},
 
 mewniumz: {
 name: "Mewnium Z",
@@ -3495,6 +3479,20 @@ pokemon.eatItem();
 },
 onEat(pokemon) {
 pokemon.addVolatile('micleberry');
+},
+condition: {
+duration: 2,
+onSourceAccuracy(accuracy, target, source, move) {
+if (!move.ohko) {
+this.add('-enditem', source, 'Micle Berry');
+source.removeVolatile('micleberry');
+if (typeof accuracy === 'number') {
+return this.chainModify([115, 100]);
+}
+}
+},
+},
+isNonstandard: "Unobtainable",
 },
 
 mimikiumz: {
@@ -5115,15 +5113,6 @@ itemUser: ["Snorlax"],
 isNonstandard: "Past",
 },
 
-if (target.eatItem()) {
-this.debug('-50% reduction');
-this.add('-enditem', target, this.effect, '[weaken]');
-return this.chainModify(0.5);
-}
-}
-},
-onEat() { },
-},
 snowball: {
 name: "Snowball",
 fling: {
@@ -5153,6 +5142,15 @@ return this.chainModify([115, 100]);
 },
 },
 
+if (target.eatItem()) {
+this.debug('-50% reduction');
+this.add('-enditem', target, this.effect, '[weaken]');
+return this.chainModify(0.5);
+}
+}
+},
+onEat() { },
+},
 solganiumz: {
 name: "Solganium Z",
 onTakeItem: false,
@@ -5979,10 +5977,6 @@ return critRatio + 1;
 
 
 
+
+
 };
-
-
-
-
-
-
